8a14475d656b2e5577d87c5e337df449c640ebcb
Anton Tatuev <koob27@gmail.com>
2014-05-20 14:20:59 +0400

    changed equality transformation

--- pddl4pddl.java
+++ pddl4pddl.java
@@ -15,6 +15,7 @@ import pddl4j.exp.AtomicFormula;
 import pddl4j.exp.Exp;
 import pddl4j.exp.ListExp;
 import pddl4j.exp.ExpID;
+import pddl4j.exp.term.TermID;
 import pddl4j.exp.InitEl;
 import pddl4j.exp.Literal;
 import pddl4j.exp.NotAtomicFormula;
@@ -192,76 +193,65 @@ static void transformEquality(PDDLObject domain, PDDLObject problem) throws Inva
 	}
 	
 	for (Iterator<ActionDef> iter = domain.actionsIterator(); iter.hasNext(); ) {
+		//todo: add durative actions supportk
 		Action action = (Action) iter.next();
-		replaceEqualityOperator(action.getPrecondition(), eq_predicate);
+		Exp precondition = replaceEqualityOperator(action.getPrecondition(), eq_predicate);
+		action.setPrecondition(precondition);
+		Exp effect = replaceEqualityOperator(action.getEffect(), eq_predicate);
+		action.setEffect(effect);
 	}
 }
 
-static void replaceEqualityOperator(Exp e, String eq_predicate) throws InvalidExpException {
+static Exp replaceEqualityOperator(Exp e, String eq_predicate) {
 	switch (e.getExpID()) {
     case AND: case OR:
         ListExp listExp = (ListExp) e;
 		for (int i = 0; i < listExp.size(); i++) {
-			if (listExp.get(i).getExpID().equals(ExpID.F_COMP)) {
-				FCompExp fcomp3 = (FCompExp) listExp.get(i);
-				if (fcomp3.getOp().equals(Comp.EQUAL) && fcomp3.getArg1().getExpID().equals(ExpID.TERM)
-													  && fcomp3.getArg2().getExpID().equals(ExpID.TERM)) {											
-					AtomicFormula predicate = new AtomicFormula(eq_predicate);
-					predicate.add(fcomp3.getArg1());
-					predicate.add(fcomp3.getArg2());
-					listExp.set(i, predicate);
-				}
-			} else {
-				replaceEqualityOperator(listExp.get(i), eq_predicate);
-			}
+			listExp.set(i, replaceEqualityOperator(listExp.get(i), eq_predicate));
 		}
-        break;
-	case FORALL:
-		ForallExp forall = (ForallExp) e;
-		if (forall.getExp().getExpID().equals(ExpID.F_COMP)) {
-			FCompExp fcomp1 = (FCompExp) forall.getExp();
-			if (fcomp1.getOp().equals(Comp.EQUAL) && fcomp1.getArg1().getExpID().equals(ExpID.TERM)
-												  && fcomp1.getArg2().getExpID().equals(ExpID.TERM)) {											
-				AtomicFormula predicate = new AtomicFormula(eq_predicate);
-				predicate.add(fcomp1.getArg1());
-				predicate.add(fcomp1.getArg2());
-				forall.setExp(predicate);
-			}
-		} else {
-			replaceEqualityOperator(forall.getExp(), eq_predicate);
-		}
-		break;
+		return listExp;
+	case FORALL: case EXIST:
+		QuantifiedExp quantr = (QuantifiedExp) e;
+		quantr.setExp(replaceEqualityOperator(quantr.getExp(), eq_predicate));
+		return quantr;
+	case WHEN:
+		WhenExp whenExp = (WhenExp) e;
+		whenExp.setCondition(replaceEqualityOperator(whenExp.getCondition(), eq_predicate));
+		whenExp.setEffect(replaceEqualityOperator(whenExp.getEffect(), eq_predicate));
+		return whenExp;
     case NOT:
         NotExp notExp = (NotExp) e;
-        if (notExp.getExp().getExpID().equals(ExpID.F_COMP)) {
-			FCompExp fcomp2 = (FCompExp) notExp.getExp();
-			if (fcomp2.getOp().equals(Comp.EQUAL) && fcomp2.getArg1().getExpID().equals(ExpID.TERM)
-												  && fcomp2.getArg2().getExpID().equals(ExpID.TERM)) {											
-				AtomicFormula predicate = new AtomicFormula(eq_predicate);
-				predicate.add(fcomp2.getArg1());
-				predicate.add(fcomp2.getArg2());
-				notExp.setExp(predicate);
-			}
-		} else {
-			replaceEqualityOperator(notExp.getExp(), eq_predicate);
-		}
-        break;
+		notExp.setExp(replaceEqualityOperator(notExp.getExp(), eq_predicate));
+		return notExp;
+	case TIMED_EXP:
+		TimedExp timedExp = (TimedExp) e;
+		timedExp.setExp(replaceEqualityOperator(timedExp.getExp(), eq_predicate));
+		return timedExp;
+	case IMPLY:
+		ImplyExp implyExp = (ImplyExp) e;
+		implyExp.setHead(replaceEqualityOperator(implyExp.getHead(), eq_predicate));
+		implyExp.setBody(replaceEqualityOperator(implyExp.getBody(), eq_predicate));
+		return implyExp;
+	case DERIVED_PREDICATE:
+		DerivedPredicate derivedPredicate = (DerivedPredicate) e;
+		derivedPredicate.setBody(replaceEqualityOperator(derivedPredicate.getBody(), eq_predicate));
+		return derivedPredicate;
 	case F_COMP:
-		FCompExp fcomp2 = (FCompExp) e;
-		if (fcomp2.getOp().equals(Comp.EQUAL) && fcomp2.getArg1().getExpID().equals(ExpID.TERM)
-											  && fcomp2.getArg2().getExpID().equals(ExpID.TERM)) {											
+		FCompExp fcomp = (FCompExp) e;
+		if (fcomp.getOp().equals(Comp.EQUAL) && fcomp.getArg1().getTermID().equals(TermID.VARIABLE) 
+                                             && fcomp.getArg2().getTermID().equals(TermID.VARIABLE)) {											
 			AtomicFormula predicate = new AtomicFormula(eq_predicate);
-			predicate.add(fcomp2.getArg1());
-			predicate.add(fcomp2.getArg2());
-			//notExp.setExp(predicate);
+			predicate.add(fcomp.getArg1());
+			predicate.add(fcomp.getArg2());
+			return predicate;
+		} else {
+			return e;
 		}
-		break;
-	case ATOMIC_FORMULA:        
-        break;
-    default:
-        throw new InvalidExpException(e.getExpID());
-    }
+	case ASSIGN_OP: case COND_EXP: 
+	case ATOMIC_FORMULA: case METRIC_EXP: case TERM:
+		return e;
+	}
+	return e;
 }
-
 
 }
